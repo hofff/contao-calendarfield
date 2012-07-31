b443575eabab797d62d48800a12179c3540c4d12
Andreas Schempp <andreas@schempp.ch>
2012-07-31 21:37:56 +0200

    Removed method to generate the date picker
    The method was a leftover from previous version where we supported
    the old Contao 2.9 datepicker.

--- FormCalendarField.php
+++ FormCalendarField.php
@@ -10,12 +10,12 @@
  * modify it under the terms of the GNU Lesser General Public
  * License as published by the Free Software Foundation, either
  * version 3 of the License, or (at your option) any later version.
- * 
+ *
  * This program is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
  * Lesser General Public License for more details.
- * 
+ *
  * You should have received a copy of the GNU Lesser General Public
  * License along with this program. If not, please visit the Free
  * Software Foundation website at <http://www.gnu.org/licenses/>.
@@ -34,46 +34,35 @@ class FormCalendarField extends FormTextField
 	public function __construct($arrAttributes=false)
 	{
 		parent::__construct($arrAttributes);
-		
+
 		if ($this->rgxp != 'datim' && $this->rgxp != 'time')
 		{
 			$this->rgxp = 'date';
 		}
 	}
-	
-	
+
+
 	public function generate()
 	{
+		$GLOBALS['TL_CSS'][] = 'plugins/datepicker/dashboard.css';
+		$GLOBALS['TL_JAVASCRIPT'][] = 'plugins/datepicker/datepicker.js';
+
 		$dateFormat = strlen($this->dateFormat) ? $this->dateFormat : $GLOBALS['TL_CONFIG'][$this->rgxp . 'Format'];
 		$dateDirection = strlen($this->dateDirection) ? $this->dateDirection : '0';
-		$dateImage = $this->dateImage;
-		$dateImageURL = $this->dateImageURL;
 		$jsEvent = $this->jsevent ? $this->jsevent : 'domready';
-		
+
 		if ($this->dateParseValue && $this->varValue != '')
 		{
 			$this->varValue = $this->parseDate($dateFormat, strtotime($this->varValue));
 		}
-		
+
 		$strBuffer = parent::generate();
-		
+
 		if ($this->readonly || $this->disabled)
 		{
 			return $strBuffer;
 		}
-		
-		return $this->generateWithDatepicker($strBuffer, $dateFormat, $dateDirection, $dateImage, $dateImageURL, $jsEvent);
-	}
-	
-	
-	/**
-	 * Generate for datepicker script since Contao 2.10
-	 */
-	protected function generateWithDatepicker($strBuffer, $dateFormat, $dateDirection, $dateImage, $dateImageURL, $jsEvent)
-	{
-		$GLOBALS['TL_CSS'][] = 'plugins/datepicker/dashboard.css';
-		$GLOBALS['TL_JAVASCRIPT'][] = 'plugins/datepicker/datepicker.js';
-		
+
 		// in the back end this is inlcuded automatically
 		if (TL_MODE == 'FE')
 		{
@@ -94,7 +83,7 @@ class FormCalendarField extends FormTextField
 				$rgxp = '';
 				break;
 		}
-		
+
 		switch( $dateDirection )
 		{
 			case '+1':
@@ -115,28 +104,28 @@ class FormCalendarField extends FormTextField
 		$intOffsetY = 0;
 
 		// icon
-		$strIcon = ($dateImageURL) ? $dateImageURL : 'plugins/datepicker/icon.gif';
+		$strIcon = ($this->dateImageURL) ? $this->dateImageURL : 'plugins/datepicker/icon.gif';
 		$arrSize = @getimagesize(TL_ROOT . '/' . $strIcon);
-			
+
 		// seems to be necessary for the backend but does only hurt in the FE
 		$style = (TL_MODE == 'BE') ? ' style="vertical-align:-6px;"' : '';
-				
-		if ($dateImage == 1) 
+
+		if ($this->dateImage == 1)
 		{
 			$dateImagePost = '<img src="' . $strIcon . '" width="' . $arrSize[0] . '" height="' . $arrSize[1] . '" alt="" class="CalendarFieldIcon" id="toggle_' . $this->strId . '"' . $style . '>';
 			$dataToggle = "\n	toggle:$$('#toggle_" . $this->strId . "'),";
-			
+
 			// make offsets configurable (useful for the front end but can be used in the back end as well)
 			$intOffsetX = (is_numeric($this->offsetX)) ? $this->offsetX : -197;
 			$intOffsetY = (is_numeric($this->offsetY)) ? $this->offsetY : -182;
 		}
-				
+
 
 		// correctly style the date format
 		$dateFormat = Date::formatToJs($dateFormat);
 
 		$strBuffer .= $dateImagePost.'
-				
+
 ' . $this->getScriptTag() . "
 window.addEvent('" . $jsEvent . "', function() {
   new Picker.Date($$('#ctrl_" . $this->strId . "'), {
@@ -150,29 +139,29 @@ window.addEvent('" . $jsEvent . "', function() {
   });
 });
 </script>";
-  
+
 		return $strBuffer;
 	}
-	
-	
+
+
 	public function validator($varInput)
 	{
 		if (strlen($this->dateFormat))
 		{
 			// Disable regular date validation
 			$this->rgxp = '';
-			
+
 			if (strlen($varInput) && !preg_match('/'. $this->getRegexp($this->dateFormat) .'/i', $varInput))
 			{
 				$objDate = new Date();
 				$this->addError(sprintf($GLOBALS['TL_LANG']['ERR']['date'], $objDate->getInputFormat($this->dateFormat)));
 			}
 		}
-		
+
 		return parent::validator($varInput);
 	}
-	
-	
+
+
 	/**
 	 * Return a regular expression that matches a particular date format
 	 * @param  string
@@ -203,19 +192,19 @@ window.addEvent('" . $jsEvent . "', function() {
 					$arrRegexp[$strFormat]['perl']  .= '(0[1-9]|[12][0-9]|3[01])';
 					$arrRegexp[$strFormat]['posix'] .= '(0[1-9]|[12][0-9]|3[01])';
 					break;
-				
+
 				// Patch month: allow 01 - 12
 				case 'm':
 					$arrRegexp[$strFormat]['perl']  .= '(0[1-9]|1[012])';
 					$arrRegexp[$strFormat]['posix'] .= '(0[1-9]|1[012])';
 					break;
-				
+
 				// Patch year: allow 1900 - 2099
 				case 'Y':
 					$arrRegexp[$strFormat]['perl']  .= '(19|20)[0-9]{2,2}';
 					$arrRegexp[$strFormat]['posix'] .= '(19|20)[[:digit:]]{2}';
 					break;
-					
+
 				case 'a':
 				case 'A':
 					$arrRegexp[$strFormat]['perl']  .= '[apmAPM]{2,2}';
@@ -252,7 +241,7 @@ window.addEvent('" . $jsEvent . "', function() {
 
 	/**
 	 * Return the datepicker string
-	 * 
+	 *
 	 * Fix the MooTools more parsers which incorrectly parse ISO-8601 and do
 	 * not handle German date formats at all.
 	 * @return string
@@ -278,15 +267,15 @@ window.addEvent("domready",function(){
 });
 </script>';
 	}
-	
-	
+
+
 	protected function getScriptTag()
 	{
 		if (TL_MODE == 'BE')
 		{
 			return '<script>';
 		}
-		
+
 		global $objPage;
 
 		return $objPage->outputFormat == 'html' ? '<script>' : '<script type="text/javascript">';

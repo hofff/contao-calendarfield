56f4579fea53993d3dd7a1d934b9bd7f6c06334b
Andreas Schempp <andreas.schempp@terminal42.ch>
2012-10-02 17:49:28 +0200

    Added autoload files for Contao 3

---
+++ config/autoload.php
@@ -0,0 +1,20 @@
+<?php
+
+/**
+ * Contao Open Source CMS
+ * 
+ * Copyright (C) 2005-2012 Leo Feyer
+ * 
+ * @package Calendarfield
+ * @link    http://contao.org
+ * @license http://www.gnu.org/licenses/lgpl-3.0.html LGPL
+ */
+
+
+/**
+ * Register the classes
+ */
+ClassLoader::addClasses(array
+(
+	'FormCalendarField' => 'system/modules/calendarfield/FormCalendarField.php',
+));

07743f3555cd27747f09c27240feba60d1a33880
Cliff Parnitzky <github@cliff-parnitzky.de>
2016-12-14 23:08:26 +0100

    Re-add deleted text

--- CT_ROOT/system/modules/hofff_calendarfield/languages/de/tl_form_field.php
+++ CT_ROOT/system/modules/hofff_calendarfield/languages/de/tl_form_field.php
@@ -14,6 +14,7 @@ $GLOBALS['TL_LANG']['tl_form_field']['dateParseValue']         = array('Standard
 $GLOBALS['TL_LANG']['tl_form_field']['dateIncludeCSS']         = array('CSS-Datei einbinden', 'Aktivieren Sie diese Option wenn Sie die jQuery UI CSS-Datei für das Popup einbinden wollen.');
 $GLOBALS['TL_LANG']['tl_form_field']['dateIncludeCSSTheme']    = array('jQuery UI Theme', 'Wählen Sie das zu verwendende jQuery UI Theme aus. Weiter Informationen dazu gibt es auf der jQuery UI Webseite <a hre="http://jqueryui.com/themeroller">http://jqueryui.com/themeroller</a>. Für die Einbindung wird eine funktionierende Internetverbindung benötigt.');
 $GLOBALS['TL_LANG']['tl_form_field']['dateImage']              = array('Kalender-Icon anzeigen', 'Klicken Sie hier um das Kalender-Icon anzuzeigen.');
+$GLOBALS['TL_LANG']['tl_form_field']['dateImageSRC']           = array('Eigenes Icon', 'Wählen Sie ein Icon aus, welches statt dem Standardbild verwendet werden soll.');
 $GLOBALS['TL_LANG']['tl_form_field']['dateDisabledWeekdays']   = array('Nicht erlaubte Wochentage', 'Wählen Sie die Wochentage aus, die nicht erlaubt sind und somit nicht auswählbar sein sollen.');
 $GLOBALS['TL_LANG']['tl_form_field']['dateDisabledDays']       = array('Nicht erlaubte Tage', 'Wählen Sie die Daten der Tage aus, die nicht erlaubt sind und somit nicht auswählbar sein sollen.');
 $GLOBALS['TL_LANG']['tl_form_field']['dateDisabledDaysDate']   = array('Datum', 'Wählen Sie das Datum des Tages aus, die nicht erlaubt ist und somit nicht auswählbar sein sollen.');

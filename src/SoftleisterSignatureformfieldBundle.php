5af62f1ed66b320f90d07a86b4f17d65ef02092a
Cliff Parnitzky <github@cliff-parnitzky.de>
2021-08-03 00:20:14 +0200

    Correctly load ressources

---
+++ src/SoftleisterSignatureformfieldBundle.php
@@ -0,0 +1,23 @@
+<?php
+
+/**
+ * Extension for Contao 4
+ *
+ * @copyright  Softleister 2019
+ * @author     Softleister <info@softleister.de>
+ * @package    contao-signature-formfield-bundle
+ *             based on jSignature (https://github.com/brinley/jSignature)
+ * @licence    MIT License
+ */
+
+namespace Softleister\SignatureformfieldBundle;
+
+use Symfony\Component\HttpKernel\Bundle\Bundle;
+
+/**
+ * Configures the Contao Signature form field bundle.
+ */
+class SoftleisterSignatureformfieldBundle extends Bundle
+{
+
+}
